36287fa21971a611cebdedb84e8e2a7833b3f09e
wrscomputers <wrscomputers@gmail.com>
2020-05-18 17:27:09 -0400

    Added enums and struts

--- 05-06-2020 Lesson Take 2.playground/Contents.swift
+++ 05-06-2020 Lesson Take 2.playground/Contents.swift
@@ -1,3 +1,34 @@
-import UIKit
+//enum - Enumeration
 
-var str = "Hello, playground"
+enum AppleProducts: String {
+    case iPhone
+    case iPad
+    case MacBook
+    case Watch
+    case iMac
+    case iMacPro
+    
+}
+
+enum PizzaCategory: String {
+    case classic
+    case specialty
+    case glutenFree
+}
+
+let specialtyPizza = PizzaCategory.specialty
+let myFavoritePizza: PizzaCategory = .classic
+
+print(myFavoritePizza)
+
+struct Pizza {
+    let name: String
+    let cost: Double
+    let category: PizzaCategory
+}
+
+let myPizza = Pizza(name: "49-er", cost: 12.99, category: .classic)
+
+print("I would like to order a \(myPizza.name)")
+
+
